8d1b899f845d8de89c355831dde0e276f0e0efbd
asorianovilches <alvaro.sorianovilches@soprasteria.com>
2018-10-23 13:31:28 +0200

    fix: Improve code

--- Formulario/formulario.js
+++ Formulario/formulario.js
@@ -8,64 +8,49 @@ correo = formulario.correo,
 sexo = formulario.sexo,
 terminos = formulario.terminos,
 error = document.getElementById('error');
+var salida = '';
 
-function validarNombre(e) {
-    if(nombre.value == '' || nombre.value == null){
-        console.log('Complete el nombre');
-        error.style.display = 'block';
-        error.innerHTML +='<li>Complete nombre</li>';
-        e.preventDefault();
-        
-    } else {
-        error.style.display = 'none';
+function validarNombre(e){
+    if(nombre.value == '' || nombre.value == null) {
+        salida += '<li>Complete nombre</li>';
     }
-
 }
 
 function validarCorreo(e) {
     if(correo.value == '' || correo.value == null) {
-        error.style.display = 'block';
-        error.innerHTML += '<li>Complete Correo Electrónico</>';
-        e.preventDefault();
-
-} else {
-    error.style.display = 'none';
-}
+        salida += '<li>Complete correo electrónico</li>';
+    }
 }
 
 function validarSexo(e) {
     if(sexo.value == '' || sexo.value == null) {
-        error.style.display = 'block';
-        error.innerHTML +='<li>Campo obligatorio sin rellenar (sexo)</li>';
-        e.preventDefault();
-    } else {
-        error.style.display = 'none';
+        salida += '<li>Complete sexo</li>';
     }
-
 }
 
 function validarTerminos(e) {
     if(terminos.checked == false) {
-        terminos.style.display = 'block';
-        error.innerHTML +='<li>Terminos no aceptados</>';
-        e.preventDefault();
-    } else {
-        error.style.display = 'none';
-    }
-
-
+        salida += '<li>Acepte los terminos</li>';
+        }
 }
 
 function validarFormulario(e) {
+    console.log('e:', e);
     error.innerHTML = '';
     validarNombre(e);
     validarCorreo(e);
     validarSexo(e);
     validarTerminos(e);
-    
+    if(salida == '' || salida == null){
+        error.style.display = 'none';
+    } else {
+        error.style.display = 'block';
+        error.innerHTML += salida;
+        salida = '';
+        e.preventDefault();
+    }
 }
 
 formulario.addEventListener('submit', validarFormulario);
-
 
 }())
